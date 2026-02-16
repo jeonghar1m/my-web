3b28e059ff0f7c0cdb36f0ff3b42823dd0a0f997
Jeong Harim <me@jeongharim.com>
2026-02-17 01:09:36 +0900

    feat: portfolio page 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
+import { Flex } from "@radix-ui/themes";
+
 const HomePage = () => {
   return (
-    <>
+    <Flex direction="column">
       <h2 className="text-2xl font-bold">안녕하세요.</h2>
       <p className="mt-8 text-center text-neutral-700">
         프론트엔드 개발자 정하림입니다.
       </p>
-    </>
+    </Flex>
   );
 };
 
---
+++ src/shared/model/portfolio.ts
@@ -0,0 +1,14 @@
+import { Dayjs } from "dayjs";
+import { ReactNode } from "react";
+
+export interface Portfolio {
+  id: number;
+  title: string;
+  thumbnailUrl?: string;
+  githubUrl?: string;
+  /** NOTE: 배포된 사이트 Url */
+  productionUrl?: string;
+  startDate?: Dayjs;
+  endDate?: Dayjs;
+  description: ReactNode;
+}
