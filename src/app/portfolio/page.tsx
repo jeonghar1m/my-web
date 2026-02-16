31983d8c08cecd6e4a98fc0dfca579451756ebf0
Jeong Harim <me@jeongharim.com>
2026-02-17 01:09:36 +0900

    feat: portfolio page 추가

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,9 +1,16 @@
+import PortfolioData from "./portfolio-data";
+import PortfolioCard from "./portfolio-card";
+
 const PortfolioPage = () => {
   return (
-    <>
+    <div>
       <h2 className="text-2xl font-bold">포트폴리오</h2>
-      <p className="mt-8 text-center text-neutral-700">준비 중입니다.</p>
-    </>
+      <div className="mt-6 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        {PortfolioData.map((portfolio) => (
+          <PortfolioCard key={portfolio.id} portfolio={portfolio} />
+        ))}
+      </div>
+    </div>
   );
 };
 
