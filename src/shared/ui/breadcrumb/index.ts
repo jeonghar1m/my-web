0e5964f3004df11c3df84a6ed804296c7bb01696
Jeong Harim <me@jeongharim.com>
2026-02-17 22:22:12 +0900

    feat: add Breadcrumb

---
+++ src/shared/ui/breadcrumb/index.ts
@@ -0,0 +1,11 @@
+export {
+  Breadcrumb,
+  BreadcrumbList,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+  BreadcrumbEllipsis,
+} from "./breadcrumb";
+export { default as BreadcrumbNav } from "./breadcrumb-nav";
+export type { BreadcrumbNavItem } from "./breadcrumb-nav";
