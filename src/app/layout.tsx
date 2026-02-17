4e2a3312870ae7e064a010821478b47dba8577f4
Jeong Harim <me@jeongharim.com>
2026-02-17 22:22:27 +0900

    refact: Header, Footer 상위 디렉토리명 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Header from "@/widgets/Header";
+import Footer from "@/widgets/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
