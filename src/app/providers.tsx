af9a6e49ca7d3a590551b14e6ff5c899784a9435
Jeong Harim <me@jeongharim.com>
2026-02-21 19:58:51 +0900

    fix: radix theme 정상 렌더링 되지 않던 문제 수정

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Theme } from "@radix-ui/themes";
 import { useState } from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <Theme>{children}</Theme>
+    </QueryClientProvider>
   );
 }
